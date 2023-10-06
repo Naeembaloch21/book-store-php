2adbd4dcdf970cffd6e43c417c08d0fac6875664
Naeembaloch21 <naeemalbaloshi21@gmail.com>
2023-10-05 22:24:10 -0700

    I created my header navbar and footer

---
+++ layouts/footer.php
@@ -0,0 +1,66 @@
+<footer class="mt-5 bg-dark text-white">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <h1>BOOK SHOP</h1>
+                <p class="mt-4">1203 Town Center</p>
+                <p>+0000 123 456 789</p>
+                <p>info@example.com</p>
+            </div>
+            <div class="col-sm-3">
+                <h1>Help</h1>
+                <p class="mt-4">Search</p>
+                <p>Help</p>
+                <p>information</p>
+                <p>Privacy Policy</p>
+                <p>Shipping Information</p>
+            </div>
+            <div class="col-sm-3">
+                <h1 class="text-">Support</h1>
+                <p class="mt-4">Search Terms</p>
+                <p>dvanced Search</p>
+                <p>Helps & Faqs</p>
+                <p>Store & Location</p>
+                <p>Orders & Returns</p>
+            </div>
+            <div class="col-sm-3">
+                <h1>Information</h1>
+                <p class="mt-4">Contact</p>
+                <p>About</p>
+                <p>Carrers</p>
+                <p>Refund & Returns</p>
+                <p>Deliveries</p>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-12 mt-5 d-flex justify-content-center">
+                <i class="fa fa-twitter " aria-hidden="true"></i>
+                <i class="fa fa-facebook-official ms-5" aria-hidden="true"></i>
+                <i class="fa fa-pinterest-p ms-5" aria-hidden="true"></i>
+                <i class="fa fa-instagram ms-5" aria-hidden="true"></i>
+                <i class="fa fa-tumblr ms-5" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    <img src="" alt="">
+    <hr style="border-color:22px white;" class="mb-5 ">
+    <div class="container">
+        <div class="row  d-flex">
+            <div class="col-sm-6">
+                <p>+ All Right Reserverd © 2023, Bookly-theme</p>
+            </div>
+            <div class="col-sm-6 d-flex justify-content-end">
+                <i class="fa fa-cc-visa text-info" aria-hidden="true"></i>
+                <i class="fa fa-cc-mastercard text-danger ms-2" aria-hidden="true"></i>
+                <i class="fa fa-cc-amex text-info ms-2" aria-hidden="true"></i>
+                <i class="fa fa-paypal text-info ms-2" aria-hidden="true"></i>
+                <i class="fa fa-cc-discover text-danger ms-2" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ layouts/navbar.php
@@ -0,0 +1,62 @@
+<div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-md-4 d-flex ">
+            <div class="text-success mr-2 me-2">
+                <i class="fa fa-shopping-cart fs-4 me-3" aria-hidden="true"></i>
+            </div>
+            <div class="mr-2 me-2">
+                <i class="fa fa-heart fs-4 me-3" ></i>
+            </div>
+            <div class="me-2">
+                <i class="fa fa-copy fs-4 me-3"></i>
+            </div>
+        </div>
+
+
+        <div class="col-md-4">
+            <img class="mx-auto d-block" src="../book-store/assets/images/logo_png (1).webp" style="width: 49.5%;" alt="">
+        </div>
+        <div class="col-md-4  text-end">
+            <div class="dropdown pe-3">
+                <button class="btn btn-danger me-3 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    US
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="#">AF</a></li>
+                    <li><a class="dropdown-item" href="#">AX</a></li>
+                    <li><a class="dropdown-item" href="#">AL</a></li>
+                </ul>
+                <a href="login.html"><i class="fa fa-user fs-4 pe-3 cursor-pointer text-success" aria-hidden="true"></i></a>
+                <i class="fa fa-search fs-4 pe-3 text-success" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-danger text-light align-items-center mt-4 p-4 ">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto d-block d-flex text-light">
+                        <li class="nav-item">
+                            <a class="nav-link active pe-5 text-light" aria-current="page" href="../../../Php-BookStore/index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active pe-5 text-light" href="../../../Php-BookStore/books.php">BOOKS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active pe-5 text-light" href="../../../Php-BookStore/about.php" tabindex="-1" aria-disabled="true">About US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active pe-5 text-light" href="../../../Php-BookStore/contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
