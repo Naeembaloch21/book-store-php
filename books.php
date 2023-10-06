fe237b75315fa5d1477c9bbb17dc2e4aaf53728d
Naeembaloch21 <naeemalbaloshi21@gmail.com>
2023-10-05 23:47:38 -0700

    all a. href have been removed

--- books.php
+++ books.php
@@ -7,145 +7,98 @@ include('layouts/navbar.php')
 <div class="container-fluid">
     <div class="row mt-5" id="demo">
         <div class="col-12 col-md-6 col-lg-3 text-center ">
-            <a href="Details Pages/DETAILS11.htm">
-                <img src="assets/images/shop-new-93.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/shop-new-93.webp" class="w-100" alt="Error:404.">
             <h3>DONALD WILLIAM</h3>
             <h4>Adventurous Eating</h4>
             <h3>$500.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/Details12.html">
-                <img src="assets/images/2.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/2.webp" class="w-100" alt="Error:404.">
             <h3>MICHAEL ALICE</h3>
             <h4>Beauty of Structures</h4>
             <h3>$310.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details13.htm">
-                <img src="assets/images/3.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/3.webp" class="w-100" alt="Error:404.">
             <h3>MICHAEL ALICE</h3>
             <h4>Books For a Cause</h4>
             <h3>$110.00</span>
             </h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details14.htm">
-                <img src="assets/images/4.webp " class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/4.webp " class="w-100" alt="Error:404.">
             <h3>DONALD WILLIAM</h3>
             <h4>Donate a Books</h4>
             <h3>$100.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
         </div>
     </div>
 </div>
 <div class="container-fluid">
     <div class="row mt-5">
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details15.htm">
-                <img src="assets/images/5.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/5.webp" class="w-100" alt="Error:404.">
             <h3>JOHN ERIK</h3>
             <h4>Endless Summer</h4>
             <h3>$210.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
             </a>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details16.htm">
-                <img src="assets/images/6.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/6.webp" class="w-100" alt="Error:404.">
             <h3>JOHN ERIK</h3>
             <h4>Fight or Flight</h4>
             <h3>$210.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details17.htm">
-                <img src="assets/images/7.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/7.webp" class="w-100" alt="Error:404.">
             <h3>BOOKLY-THEME</h3>
             <h4>Fresh HealthyError:404. Eats</h4>
             <h3>$110.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details18.htm">
-                <img src="assets/images/4.png" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/4.png" class="w-100" alt="Error:404.">
             <h3>ALICE JAMES</h3>
             <h4>Girls at the Golden City</h4>
             <h3>$300.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
         </div>
     </div>
 </div>
 <div class="container-fluid">
     <div class="row mt-5">
         <div class="col-12 col-md-6 col-lg-3 text-center ">
-            <a href="Details Pages/details19.htm">
-                <img src="assets/images/9.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/9.webp" class="w-100" alt="Error:404.">
             <h3>ALICE JAMES</h3>
             <h4>Home Made Meals</h4>
             <h3>$310.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details20.htm">
-                <img src="assets/images/1.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/1.webp" class="w-100" alt="Error:404.">
             <h3>MICHAEL ALICE</h3>
             <h4>the Red Planet</h4>
             <h3>$500.00 </h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details21.htm">
-                <img src="assets/images/11.webp" class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/11.webp" class="w-100" alt="Error:404.">
             <h3>MICHAEL ALICE</h3>
             <h4>Lightspeed Robots</h4>
             <h3>$400.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-3">ADD TO CARD</button>
         </div>
         <div class="col-12 col-md-6 col-lg-3 text-center">
-            <a href="Details Pages/details22.htm">
-                <img src="assets/images/12.webp " class="w-100" alt="Error:404.">
-            </a>
+            <img src="assets/images/12.webp " class="w-100" alt="Error:404.">
             <h3>ADAM BAID</h3>
             <h4>Seafood Gourment</h4>
             <h3>$500.00</h3>
-            <a href="table.htm">
-                <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
-            </a>
+            <button class="border-0 bg-danger text-light p-2 mb-">ADD TO CARD</button>
         </div>
     </div>
 </div>
