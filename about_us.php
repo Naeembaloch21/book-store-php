0bb96df60b745b7afea9cebfc6af9c2af1b4e209
Naeembaloch21 <naeemalbaloshi21@gmail.com>
2023-10-08 22:18:37 -0700

    about_us file created

---
+++ about_us.php
@@ -0,0 +1,130 @@
+<?php
+include('layouts/header.php')
+?>
+<?php
+include('layouts/navbar.php')
+?>
+
+<div class="container">
+    <div class="row pt-5 pb-5 bg-light">
+        <div class="col-md-6">
+            <h2>About Us 01</h2>
+        </div>
+        <div class="col-md-6 d-flex justify-content-end align-items-center">
+            <p class="m-0">Home / About Us 01</p>
+        </div>
+    </div>
+</div>
+<div class="d-flex justify-content-center mt-5">
+</div>
+<h1 class="text-center mt-3">Welcom to Auteur</h1>
+<h3 class="text-center mt-3">A MONTHLY BOOKS REVIEW PUBLICATION</h3>
+<div class="container mt-5">
+    <p>Imagine two men facing each other, pointing past one another. One is pointing at a tornado that is coming,
+        and the other at a raging fire headed towards them. Each sees their own truth and is angry at the sight of
+        the other’s hand. Each feels
+        that the other’s hand is “wrong.” This may seem silly, but replace the tornado and fire with any modern
+        issues, and the hands with words, and this scene describes how we often try to communicate.</p>
+    <div class="d-flex mt-5">
+        <h3 class="ms-5">We point past each other with our words, arguing as though we are looking at the same facts
+            and experiences.</h3>
+    </div>
+    <p class="mt-5">We want to prove our words are the right ones, instead of learning to look at what the other’s
+        words are pointing at. Words are seductive, and for all their undeniable usefulness, they also can lead us
+        away from understanding when we focus on
+        them, when we make them more important than the truth they are meant to point at.</p>
+</div>
+<div class="container">
+    <video src="https://www.youtube.com/watch?v=eH2WNtL5ong "></video>
+    <div class="container-fluid mb-5 ">
+        <div class="row ">
+            <div class="col-sm-4 mt-4">
+                <img src="assets/images/single-image-16.jpg " width="100% " alt="Error:404">
+            </div>
+            <div class="col-sm-4 mt-4 ">
+                <img src="assets/images/single-image-18.jpg " width="100% " alt="Error:404">
+            </div>
+            <div class="col-sm-4 mt-4">
+                <p>Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit
+                    estibulum issim Lorem ipsum dolor sit amet, AKON lsicn íoifa lkoix consectetur cing elit.</p>
+                <p class="fw-bold ">ADDRESS</p>
+                <i>1800 Abbot Kinney Blvd. Unit D & E Venice</i>
+                <p class="fw-bold ">PHONE</p>
+                <i>Mobile: (+88) – 1990 – 6886</i>
+                <p><i>Hotline: 1800 – 1102</i></p>
+                <p class="fw-bold ">Email</p>
+                <p>contact@auteur.com</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid " style="background-color: rgba(211, 211, 211, 0.253); ">
+    <h3 class="text-danger text-center ">TESTIMONIALS</h3>
+    <h1 class="text-center ">Read Reviews by My Readers</h1>
+    <div class="container ">
+        <div class="row ">
+            <div class="col-sm-6 mt-5 ">
+                <img class="rounded-circle " src="assets/images/testimonial-01-140x140.jpg " alt="Error:404">
+                <p class="fw-bold ">" Auteur is a monthly book review publication distributed to 400,000 avid
+                    readers through subscribing bookstores & public libraries. "</p>
+                <p class="fw-bold ">Vladimir Nabokov</p>
+                <p>/Reporter</p>
+            </div>
+            <div class="col-sm-6 mt-5 ">
+                <img class="rounded-circle " src="assets/images/testimonial-02-140x140.jpg " alt="Error:404">
+                <p class="fw-bold ">" It was a dark night, with only occasional scattered lights, glittering like
+                    stars on the plain. It flashed upon me suddenly: they were going to shoot me! "</p>
+                <p class="fw-bold ">Savanna Walker</p>
+                <p>/ Reporter</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container mt-5 ">
+    <div class="row border border-3 ">
+        <div class="col-sm-6 mt-5 ">
+            <h1 class="text-center ">Join the community</h1>
+            <p class="text-center ">News to get in touch</p>
+            <div class="mb-5 mt-5 ">
+                <div>
+                    <input type="email " class="form-control border-0 text-center mt-5 d-flex fle " id="email " placeholder="Enter email " name="email ">
+                </div>
+                <hr>
+                <div class="text-center ">
+                    <button class="border-0 bg-danger fw-bold ">Submit</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 mt-5 d-flex ">
+            <div class="me-5 mt-5 ">
+                <img src="assets/images/single-image-06.png " alt="Error:404" width="100% " height="45% ">
+            </div>
+            <div class="ms-5 ">
+                <img src="assets/images/single-image-07.png " alt="Error:404" width="100% " height="76% ">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container mt-5 ">
+    <div class="row ">
+        <div class="col-sm-3 ">
+            <h1>80k</h1>
+            <h4>ACTIVE READERS</h4>
+        </div>
+        <div class="col-sm-3 ">
+            <h1>+3k</h1>
+            <h4>TOTAL PAGES</h4>
+        </div>
+        <div class="col-sm-3 ">
+            <h1>283</h1>
+            <h4>CUP OF COFFEE</h4>
+        </div>
+        <div class="col-sm-3 ">
+            <h1>14K</h1>
+            <h4>FACEBOOK FANS</h4>
+        </div>
+    </div>
+</div>
+<?php
+include('layouts/footer.php')
+?>
